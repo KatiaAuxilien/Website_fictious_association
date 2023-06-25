3a0985d8989a444cbc833f3dd87be64b4bf35215
K. Auxilien <125131297+KatiaAuxilien@users.noreply.github.com>
2023-06-25 17:56:12 +0200

    Commit reorganisation 2

--- Sae_Achok_Flavio_Katia/views/footer.php
+++
@@ -1,7 +0,0 @@
-</body>
-<footer class="footer">
-  <p> Il s'agit d'un site web fictif pour une association fictive réalisé dans le cadre d'un projet étudiant.
-  <a href=./cgu.php>Conditions générales d'utilisation </a></p>
-</footer>
-<br><br><br><br><br><br><br><br>
-</html>
